3a6452aecf5f05f29b429365326970271338756a
eascarrunz <eascarrunz.dev@mailfence.com>
2026-03-05 00:44:41 +0100

    add std deviation, covariance, Welford's algorithm

--- pkg/stats/stats.go
+++ pkg/stats/stats.go
@@ -1,6 +1,7 @@
 package stats
 
 import (
+	"fmt"
 	"math"
 	"slices"
 )
@@ -11,26 +12,70 @@ type Numeric interface {
 
 // Computes the variance of a slice of numeric values (int or float64)
 func Variance[T Numeric](X *[]T, sample bool) float64 {
-	if len(*X) < 2 {
+	n := len(*X)
+	if n < 2 {
 		return 0.0
 	}
 
-	K := float64((*X)[0]) // Shift constant
-	var Ex, Ex2 float64
-
-	for _, x := range *X {
-		xfloat := float64(x)
-		Ex += xfloat - K
-		Ex2 += math.Pow(xfloat-K, 2)
+	var mean, M2 float64
+	for i, x := range *X {
+		delta := float64(x) - mean
+		mean += delta / float64(i+1)
+		M2 += delta * (float64(x) - mean)
 	}
 
-	n := float64(len(*X))
-	denom := n
+	denom := float64(n)
+
+	// Bessel's correction
 	if sample {
 		denom -= 1.0
 	}
+	return M2 / denom
+}
 
-	return (Ex2 - math.Pow(Ex, 2)/n) / denom
+// Computes the standard deviation of numeric values (int or float64)
+// The simple biased correction is used for the sampled variant
+func StandardDeviation[T Numeric](X *[]T, sample bool) float64 {
+	return math.Sqrt(Variance(X, sample))
+}
+
+func Covariance[T Numeric](X *[]T, Y *[]T, sample bool) float64 {
+	if len(*X) != len(*Y) {
+		panic(fmt.Sprintf("cannot compute covariance between vectors of different lengths: %d and %d", len(*X), len(*Y)))
+	}
+	n := len(*X)
+	if n < 2 {
+		return 0
+	}
+
+	var meanX, meanY, C float64
+	for i := range *X {
+		x := float64((*X)[i])
+		y := float64((*Y)[i])
+		dx := x - meanX
+		meanX += dx / float64(i+1)
+		meanY += (y - meanY) / float64(i+1)
+		C += dx * (y - meanY)
+	}
+
+	denom := float64(n)
+
+	// Bessel's correction
+	if sample {
+		denom -= 1.0
+	}
+	return C / denom
+}
+
+// Return the Pearson correlation coefficient between two variables
+func CorrPearson[T Numeric](X *[]T, Y *[]T, sample bool) float64 {
+	rho := Covariance(X, Y, sample)
+	rho /= StandardDeviation(X, sample) * StandardDeviation(Y, sample)
+
+	rho = min(rho, 1.0)
+	rho = max(rho, -1.0)
+
+	return rho
 }
 
 // Computes the sum of a slice of float64 values using Kahan's compensation algorithm
@@ -65,6 +110,9 @@ func MeanInt(X *[]int) float64 {
 
 // Computes the median of a slice of numeric values (int or float64)
 func Median[T Numeric](X *[]T) float64 {
+	if len((*X)) == 0 {
+		panic("cannot compute median of an empty array")
+	}
 	tmp := slices.Clone(*X)
 	slices.Sort(tmp)
 
@@ -79,6 +127,9 @@ func Median[T Numeric](X *[]T) float64 {
 // Returns the minimum value in a slice of int or float values
 func Minimum[T Numeric](X *[]T) T {
 	N := len(*X)
+	if N == 0 {
+		panic("cannot compute minimum of an empty array")
+	}
 	if N == 1 {
 		return (*X)[0]
 	}
@@ -96,6 +147,9 @@ func Minimum[T Numeric](X *[]T) T {
 // Returns the maximum value in a slice of int or float values
 func Maximum[T Numeric](X *[]T) T {
 	N := len(*X)
+	if N == 0 {
+		panic("cannot compute maximum of an empty array")
+	}
 	if N == 1 {
 		return (*X)[0]
 	}
